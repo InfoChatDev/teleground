787b3c513048d88e911314801d268d5e25fca824
opzc35 <opzc35@outlook.com>
2025-04-16 08:17:26 +0000

    Finish #9

--- index.py
+++ index.py
@@ -1,9 +1,20 @@
 from flask import *
+import atexit
+from json import *
+
 app = Flask(__name__)
-chat_list = ["114514","asf","asdf","asdf"]
+
+chat_list = []
+
+def exit_run():
+    content = dumps(chat_list)
+    with open("saved.json","w")as f:
+        f.write(content)
+
 @app.route('/')
 def index():
     return "Welcome to Teleground"
+
 @app.route('/chat/',methods=['GET','POST'])
 def chat():
     if request.method == 'GET':
@@ -12,5 +23,7 @@ def chat():
         content = request.form.get("content")
         print(content)
         chat_list.append(content)
+        exit_run()
         return redirect("/chat/")
-app.run('0.0.0.0',8000,True)
+if __name__ == '__main__':
+    app.run('0.0.0.0',8000,True)
