af39aaf54158ac23ecda8ac7d5eb362027f0539f
opzc35 <opzc35@gmail.com>
2025-05-03 09:36:45 +0800

    Update index.py

--- index.py
+++ index.py
@@ -18,17 +18,15 @@ def index():
 @app.route('/chat_anymous/',methods=['GET','POST'])
 def chat():
     if request.method == 'GET':
-        return render_template("chat.html",chat_list=chat_list)
+        return render_template("chat_anymous.html",chat_list=chat_list)
     else:
         content = request.form.get("content")
         print(content)
         chat_list.append(content)
         exit_run()
         return redirect('/chat_anymous/')
+      
 if __name__ == '__main__':
-    try:
-        with open("saved.json","r")as f:
-            chat_list = json.loads(f.read())
-    except:
-        chat_list = []
+    with open("saved.json","r")as f:
+        chat_list = json.loads(f.read())
     app.run('0.0.0.0',8000,False)
